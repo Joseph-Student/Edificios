1a216e105526a9f7607516c02f44cf26f9cd4efa
Joseph Perez <24597827@e.ne.udo.edu.ve>
2017-02-06 01:12:00 -0400

    Resuelto el juego de una matriz 2X2.

--- Edificios.pl
+++ Edificios.pl
@@ -9,12 +9,12 @@ checkSolution([N,S,E|Vs],M):- checkViews(E,M), invertirMatriz(M,Oe), head(Vs,O),
 
 % Verifica las vista con su solucion.
 checkViews([],[]).
-checkViews([_],[]).
+checkViews([_],[]):-!.
 checkViews([V|Vs],[S|Ss]):- checkView(V,S), checkViews(Vs,Ss).
 
 % Verifica una vista con su respectiva fila de solucion
 checkView(_,[]).
-checkView(N,F):- viewEdifice(0,F,V), V =< N.
+checkView(N,F):- viewEdifice(0,F,V), V == N.
 
 % Predicado que devuelve cuantos edificios segun una perspectiva.
 % Recibe el edificio anterior(Y), una perspectiva(P) y devuelve el numero de edificios que se ven(N).
@@ -24,7 +24,7 @@ viewEdifice(Y,[X|Xs],N):- X > Y, viewEdifice(X,Xs,R), N is R + 1, !; viewEdifice
 
 % Invierte una matriz
 invertirMatriz([],[]).
-invertirMatriz([X|Xs],[Y|Ys]):- invertir(X,Y), invertirMatriz(Xs,Ys).
+invertirMatriz([X|Xs],[Y|Ys]):- invertir(X,Y), invertirMatriz(Xs,Ys),!.
 
 % Invierte una lista.
 invertir([X],[X]).
@@ -32,7 +32,7 @@ invertir([X|M],Z):- invertir(M,S), concatenar(S,[X],Z),!.
 
 % concatena dos listas.
 concatenar([],L,L).
-concatenar([X|M],L,[X|Z]):- concatenar(M,L,Z).
+concatenar([X|M],L,[X|Z]):- concatenar(M,L,Z),!.
 
 % Obtiene la N columna de una matriz.
 column([],_,[]).
@@ -40,32 +40,32 @@ column([X|_],0,X):-!.
 column([_|Xs],N,Ys):- M is N - 1, column(Xs,M,Ys),!.
 
 % Obtine la primera columna de una matriz.
-columns([],_,[]).
-columns([X|Xs],N,Z):- column(X,N,Y), columns(Xs,N,Ys), (Y == [], concatenar(Y,Ys,Z),!; concatenar([Y],Ys,Z)).
+columns([],_,[]):-!.
+columns([X|Xs],N,Z):- column(X,N,Y), columns(Xs,N,Ys), (Y == [], concatenar(Y,Ys,Z),!; concatenar([Y],Ys,Z)),!.
 
 % Obtiene una matriz con todas las columnas.
 getColumns([[]|_],[]).
 getColumns(X,[Y|Ys]):- columns(X,0,Y), sacaPriMatriz(X,R), getColumns(R,Ys),!.
 
 % Obtiene una fila dada.
-getRow([],_,[]).
-getRow([X],0,X).
+getRow([],_,[]):-!.
+getRow([X|_],0,X):-!.
 getRow([_|Xs],N,Y):- N1 is N -1, getRow(Xs,N1,Y),!.
 
 % Saca el primer elemento de una lista.
 sacaPri([],[]).
-sacaPri([_|M],M).
+sacaPri([_|M],M):-!.
 
 % Saca todos los primeros elemntos de una matriz.
 sacaPriMatriz([],[]).
-sacaPriMatriz([X|Xs],[Y|Ys]):- sacaPri(X,Y), sacaPriMatriz(Xs,Ys).
+sacaPriMatriz([X|Xs],[Y|Ys]):- sacaPri(X,Y), sacaPriMatriz(Xs,Ys),!.
 
 % Obtiene la cabeza de una lista.
 head([X|_],X).
 
 % Devuelve el tamaño de una lista
 sizeList([],0).
-sizeList([_|Xs],N):- sizeList(Xs,N1), N is N1 + 1.
+sizeList([_|Xs],N):- sizeList(Xs,N1), N is N1 + 1,!.
 
 % Tamaño de pisos de los edificios.
 value(1).
@@ -84,13 +84,13 @@ list([X|_]):- value(M), X == M,!.
 
 
 % Agrega el valor a la solucion.
-addValue([],_,Num,[[Num]]).
+addValue([],_,Num,[[Num]]):-!.
 addValue([H|T],Size,Num,L):- sizeList(H,Y), Y == Size, addValue(T,Size,Num,Z), (list(Z), concatenar([H],[Z],L); concatenar([H],Z,L)),!.
 addValue([H|_],Size,Num,[L]):- sizeList(H,Y), Y < Size, concatenar(H,[Num],L).
 
 
 % Si todos los elementos de la lista son diferentes entre si.
-diferente(_,[]).
+diferente(_,[]):-!.
 diferente(N,[H|T]):- N \== H, diferente(N,T),!.
 
 /**
@@ -99,13 +99,20 @@ diferente(N,[H|T]):- N \== H, diferente(N,T),!.
  * 2.- En la columna no puede haber un edificio del mismo tamaño.
  * Recibira la vista de una fila(V) y su respectiva columna(C).
  **/
-comprobarValor(V,F,C,N):- diferente(N,F), diferente(N,C), concatenar(F,[N],L), viewEdifice(0,L,T), T =< V.
+comprobarValor(V,S,Nf,Nc,N):- getRow(S,Nf,F), diferente(N,F), columns(S,Nc,C), diferente(N,C), getRow(V,2,V3), 
+							column(V3,Nf,E), concatenar(F,[N],L), viewEdifice(0,L,T), T =< E.
 
 % Resuelve el juego.
-%resolver(V,S):- not(checkSolution(V,S)), resolver().
-resolver(V,Size,Nc,Nf,S):- getRow(S,Nf,F), columns(S,Nc,C), getRow(V,3,V3), column(V3,Nc,E), value(Num), Num =< Size,
-							comprobarValor(E,F,C,Num), addValue(S,Size,Num,R), N1 is Nc + 1, fila(Nc,Size,Nf,B),
-							checkSolution(V,R), resolver(V,Size,N1,B,R).
+resolver(V,_,_,_,S):- checkSolution(V,S), mostrar(S),!.
+resolver(V,Size,Nc,Nf,S):- value(Num), Num =< Size, comprobarValor(V,S,Nf,Nc,Num), addValue(S,Size,Num,R), 
+							fila(Nc,Size,Nf,B), columna(Nc,Size,T), resolver(V,Size,T,B,R),!.
 
+% indica en que fila va.
+fila(N,M,S,B):- (K is M - 1, N == K, B is S + 1); B is S,!.
 
-fila(N,M,S,B):- N == M, B is S + 1; B is S.
+% Indica en que columna va.
+columna(N,M,B):- (K is M - 1, N == K , B is 0); B is N +1,!.
+
+% muestra todas las listas de la matriz
+mostrar([]) :- write("\n").		
+mostrar([X|Xs]) :-  write(X), write("\n"), mostrar(Xs).
